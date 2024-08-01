2ee18aa4e7f3573a30a9ab9a58498434d825839b
Site Backup shtychki.by <info@shtychki.by>
2024-08-01 11:34:02 +0300

    01.08.24 -dev --update

--- local/templates/aspro_max/components/bitrix/catalog.search/main/bitrix/search.page/.default/lang/en/template.php
+++ local/templates/aspro_max/components/bitrix/catalog.search/main/bitrix/search.page/.default/lang/en/template.php
@@ -1,5 +1,4 @@
-<?
-$MESS["SEARCH_GO"] = "Найти";
-$MESS["CT_BSP_ADDITIONAL_PARAMS"] = "Дополнительные параметры поиска";
-$MESS["CT_BSP_KEYBOARD_WARNING"] = "В запросе \"#query#\" восстановлена раскладка клавиатуры.";
-?>
+<?php
+$MESS["CT_BSP_ADDITIONAL_PARAMS"] = "Additional search options";
+$MESS["CT_BSP_KEYBOARD_WARNING"] = "The keyboard layout has been restored in the \"#query#\" query.";
+$MESS["SEARCH_GO"] = "Search";
